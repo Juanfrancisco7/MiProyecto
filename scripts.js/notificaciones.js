beeafb38b0f58b03a1a1754fa84f88796f0a0b0e
Juanfrancisco7 <juanbencomo25@gmail.com>
2025-02-14 10:00:58 +0100

    ACT

--- scripts.js/notificaciones.js
+++ scripts.js/notificaciones.js
@@ -1,30 +1,32 @@
-// Verificamos si ya se ha recargado la página
-if (sessionStorage.getItem('pageReloaded')) {
-    // Mostrar la notificación después de la recarga
-    mostrarNotificacionRecarga();
-    sessionStorage.removeItem('pageReloaded'); // Limpiar la variable de sesión
+document.addEventListener("DOMContentLoaded", () => {
+  // Verificamos si la página se ha recargado
+  if (sessionStorage.getItem('pageReloaded')) {
+      mostrarNotificacion(); // Muestra la notificación inmediatamente
+      sessionStorage.removeItem('pageReloaded'); // Limpiar la variable de sesión
+  } else {
+      // Si es la primera vez que se abre la página, mostrar después de 10 segundos
+      setTimeout(mostrarNotificacion, 10000); // 10 segundos
+      sessionStorage.setItem('pageReloaded', 'true'); // Marcar para la próxima recarga
   }
-  
-  // Función para mostrar la notificación después de la recarga
-  function mostrarNotificacionRecarga() {
-    const notificacionRecarga = document.createElement("div");
-    notificacionRecarga.classList.add("notificacion-recarga");
-    notificacionRecarga.innerHTML = `
+});
+
+// Función para mostrar la notificación
+function mostrarNotificacion() {
+  // Evita notificaciones duplicadas
+  if (document.querySelector(".notificacion-recarga")) return;
+
+  const notificacion = document.createElement("div");
+  notificacion.classList.add("notificacion-recarga");
+  notificacion.innerHTML = `
       <p>¿Ya visitaste <a href="https://www.mundojuan.com" target="_blank">MUNDOJUAN</a>? 
       Cuéntame tu experiencia <a href="https://www.mundojuan.com" target="_blank">aquí</a></p>
-    `;
-    
-    // Añadimos la notificación al cuerpo del documento
-    document.body.appendChild(notificacionRecarga);
+  `;
   
-    // Eliminar la notificación después de 8 segundos
-    setTimeout(() => {
-      notificacionRecarga.remove();
-    }, 8000); // 8 segundos para eliminar la notificación
-  }
-  
-  // Establecer que la página se ha recargado para mostrar la notificación
-  if (!sessionStorage.getItem('pageReloaded')) {
-    sessionStorage.setItem('pageReloaded', 'true');
-  }
+  // Añadir la notificación al cuerpo del documento
+  document.body.appendChild(notificacion);
+
+  // Eliminar la notificación después de 8 segundos
+  setTimeout(() => notificacion.remove(), 8000);
+}
+
   
